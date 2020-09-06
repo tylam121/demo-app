06b19735186fd0dc190198d5b114c602b3290004
Tim Ying Lam <timyinglam@Tim.local>
2020-09-06 15:42:27 +0800

    Updates

---
+++ precache-manifest.5e1e41ac4ac23ba3b7716fc2ea72465c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caae62bbcce2525761c3e3bd6f368d7c",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "9e9721bf935a3ae69d10",
+    "url": "/demo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ac0184666dc72fb79a4",
+    "url": "/demo-app/static/js/2.4250e7d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-app/static/js/2.4250e7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e9721bf935a3ae69d10",
+    "url": "/demo-app/static/js/main.ca9895cc.chunk.js"
+  },
+  {
+    "revision": "316e07173b3db218f6b2",
+    "url": "/demo-app/static/js/runtime-main.46688688.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
